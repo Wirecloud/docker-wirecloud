ac7dd5420bb366bbd1830b66ddf5263a636bd365
Álvaro Arranz <aarranz@fi.upm.es>
2015-09-27 23:55:31 +0200

    Dockerfiles cleanup

--- 0.6/entrypoint.sh
+++ 0.6/entrypoint.sh
@@ -5,7 +5,5 @@
 sed -i "s/SECRET_KEY = 'TOCHANGE_SECRET_KEY'/SECRET_KEY = '` python -c "from django.utils.crypto import get_random_string; print(get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789%^&*(-_=+)'))"`'/g" /opt/wirecloud_instance/wirecloud_instance/settings.py
 
 # Start apache processes in foreground
-echo Stopping already running
 /usr/sbin/apache2ctl graceful-stop
-echo Starting apache.
 exec /usr/sbin/apache2ctl -D FOREGROUND
