c77fd217ec058b2b6eb44cd8c8a647c54e94f1cd
Álvaro Arranz <aarranz@fi.upm.es>
2016-05-19 21:12:21 +0200

    Migrate the database and collect static files in the docker-entrypoint.sh file

--- 0.8/docker-entrypoint.sh
+++ 0.8/docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# python manage.py migrate                  # Apply database migrations
-# python manage.py collectstatic --noinput  # Collect static files
 
 sed -i "s/SECRET_KEY = 'TOCHANGE_SECRET_KEY'/SECRET_KEY = '$(python -c "from django.utils.crypto import get_random_string; import re; print(re.escape(get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789%^&*(-_=+)')))")'/g" /opt/wirecloud_instance/wirecloud_instance/settings.py
+
+# Check if there are any pending migration
+python manage.py syncdb --migrate
+
+# Collect static files so we take into account custom themes and configurations
+python manage.py collectstatic --noinput
 
 # Start apache processes in foreground
 /usr/sbin/apache2ctl graceful-stop
