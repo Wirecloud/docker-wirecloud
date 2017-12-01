9481fa84b7f62b74dcea6a25f123448812a91f7f
Álvaro Arranz <aarranz@ficodes.com>
2017-12-01 09:28:21 +0100

    Create a docker-compose.yml file for the dev-composable version

--- dev-composable/docker-entrypoint.sh
+++ dev-composable/docker-entrypoint.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
-sed -i "s/SECRET_KEY = 'TOCHANGE_SECRET_KEY'/SECRET_KEY = '$(python -c "from django.utils.crypto import get_random_string; import re; print(re.escape(get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789%^&*(-_=+)')))")'/g" /opt/wirecloud_instance/wirecloud_instance/settings.py
+if [ ! -f /opt/wirecloud_instance/wirecloud_instance/settings.py ]; then
+
+    cd /opt
+
+    # Use the target_directory parameter to indicate that we want to use that
+    # directory even if it exit
+    wirecloud-admin startproject wirecloud_instance wirecloud_instance
+    chown -R wirecloud:wirecloud wirecloud_instance; \
+    chmod a+x wirecloud_instance/manage.py
+
+    cd /opt/wirecloud_instance
+
+    sed -i "s/'ENGINE': 'django.db.backends.'/'ENGINE': 'django.db.backends.postgresql_psycopg2'/g" wirecloud_instance/settings.py; \
+    sed -i "s/'NAME': ''/'NAME': 'postgres'/g" wirecloud_instance/settings.py; \
+    sed -i "s/'USER': ''/'USER': 'postgres'/g" wirecloud_instance/settings.py; \
+    sed -i "s/'PASSWORD': ''/'PASSWORD': 'postgres'/g" wirecloud_instance/settings.py; \
+    sed -i "s/'HOST': ''/'HOST': 'postgres'/g" wirecloud_instance/settings.py; \
+    sed -i "s/'PORT': ''/'PORT': '5432'/g" wirecloud_instance/settings.py; \
+    sed -i "s/SECRET_KEY = '[^']\+'/SECRET_KEY = '$(python -c "from django.utils.crypto import get_random_string; import re; print(re.escape(get_random_string(50, 'abcdefghijklmnopqrstuvwxyz0123456789%^&*(-_=+)')))")'/g" wirecloud_instance/settings.py; \
+    sed -i "s/STATIC_ROOT = path.join(BASEDIR, '..\/static')/STATIC_ROOT = '\/var\/www\/static'/g" wirecloud_instance/settings.py
+
+    python manage.py collectstatic --noinput; \
+    chown -R wirecloud:wirecloud /var/www/static
+fi
+
 
 case "$1" in
     initdb)
