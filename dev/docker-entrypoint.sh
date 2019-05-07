87cbede0638900bb7171083961a2a74136676adc
Jason Fox <jason.fox@fiware.org>
2019-05-07 17:03:16 +0200

    Revers commit

--- dev/docker-entrypoint.sh
+++ dev/docker-entrypoint.sh
@@ -2,7 +2,32 @@
 
 set -e
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'DB_PASSWORD' 'example'
+# (will allow for "$DB_PASSWORD_FILE" to fill in the value of
+#  "$DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
 
+file_env 'DB_PASSWORD' 'postgres'
+file_env 'DB_USERNAME' 'postgres'
+file_env 'SOCIAL_AUTH_FIWARE_KEY'
+file_env 'SOCIAL_AUTH_FIWARE_SECRET'
 
 # allow the container to be started with `--user`
 if [ "$(id -u)" = '0' ]; then
